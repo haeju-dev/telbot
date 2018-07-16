ffe2ea7daf253f72c65383cf8dfa687b32319d15
김민 <min8489@gmail.com>
2018-07-17 00:53:57 +0900

    make a bot

--- 1111.py
+++ 1111.py
@@ -1,9 +1,8 @@
+from pprint import pprint
 import telepot
 
 bot = telepot.Bot('694271764:AAGOyAluTq1Lq9KyHnZpDIzT4pUUMKNhGKU')
-print(bot.getMe())
 
 
-from pprint import pprint
 response = bot.getUpdates()
 pprint(response)
